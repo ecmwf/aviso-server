7ac49c302b1a94198331d4622baf14950130a1b9
sametd <samet.demir@ecmwf.int>
2025-05-24 23:36:10 +0200

    route handling for opes and api separated

--- src/startup.rs
+++ src/startup.rs
@@ -1,9 +1,13 @@
-use crate::configuration::Settings;
-use crate::routes::health_check::health_check;
-use actix_web::dev::Server;
-use actix_web::{App, HttpServer, web};
-use std::net::TcpListener;
+use std::{net::TcpListener, sync::Arc};
+
+use actix_web::{App, HttpServer, dev::Server, web};
 use tracing_actix_web::TracingLogger;
+
+use crate::{
+    configuration::Settings,
+    notification_backend::{NotificationBackend, build_backend},
+    routes::{health_check::health_check, notify::notify},
+};
 
 pub struct Application {
     port: u16,
@@ -19,8 +23,15 @@ impl Application {
         );
         let listener = TcpListener::bind(&address)?;
         let port = listener.local_addr()?.port();
-
-        let server = run(listener)?;
+        // notification notification_backend
+        let notification_backend = match build_backend(&configuration.notification_backend).await {
+            Ok(backend) => backend,
+            Err(e) => {
+                tracing::error!("Failed to initialize notification backend: {e}");
+                return Err(std::io::Error::new(std::io::ErrorKind::Other, e));
+            }
+        };
+        let server = run(listener, notification_backend)?;
         Ok(Self { port, server })
     }
 
@@ -29,22 +40,35 @@ impl Application {
     pub fn port(&self) -> u16 {
         self.port
     }
-
     // This function is used to run the server
     pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
         self.server.await
     }
 }
 
+/// Configure operational/infrastructure routes
+fn configure_ops_routes(cfg: &mut web::ServiceConfig) {
+    cfg.route("/health", web::get().to(health_check));
+}
+
+/// Configure API v1 routes
+fn configure_api_v1(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::scope("/api/v1").route("/notification", web::post().to(notify)));
+}
+
 // Run the server
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(
+    listener: TcpListener,
+    notification_backend: Arc<dyn NotificationBackend>,
+) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
-            .route("/health", web::get().to(health_check))
+            .configure(configure_ops_routes)
+            .configure(configure_api_v1)
+            .app_data(web::Data::new(notification_backend.clone()))
     })
     .listen(listener)?
     .run();
-
     Ok(server)
 }
