00c7a1a7ffa20388ff991a5aba28000314bc2360
sametd <samet.demir@ecmwf.int>
2025-05-30 00:00:41 +0200

    backend handler and more functions for notification

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -3,8 +3,10 @@
 //! This module contains functions that orchestrate between different
 //! domain modules (cloudevents, notification) and HTTP concerns
 
+pub mod backend;
 pub mod cloudevent;
 pub mod notification;
 
+pub use backend::save_to_backend;
 pub use cloudevent::process_cloudevent;
 pub use notification::process_aviso;
